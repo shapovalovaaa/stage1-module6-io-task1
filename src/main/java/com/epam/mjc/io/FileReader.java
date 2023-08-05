0b34b1229ae16adb1877e25b0efc6705818ca8a4
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-05 14:44:49 +0300

    feat: solve task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,26 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file){
+        String name;
+        String age;
+        String email;
+        String phone;
+        try (BufferedReader bf = new BufferedReader(new java.io.FileReader(file))) {
+            name = bf.readLine().split(": ")[1];
+            age = bf.readLine().split(": ")[1];
+            email = bf.readLine().split(": ")[1];
+            phone = bf.readLine().split(": ")[1];
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
     }
 }
